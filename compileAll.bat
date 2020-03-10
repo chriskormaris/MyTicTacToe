220f0aae793b4acddf2d741673a02f2e16fd9bb3
Christos Kormaris <chriskormaris@gmail.com>
2020-03-10 12:28:38 +0200

    Update compileAll.bat
    Minor changes. Replace "mkdir bin" with "if not exist bin mkdir bin". Remove unneeded "\", "." and ";" characters.

--- compileAll.bat
+++ compileAll.bat
@@ -1,6 +1,6 @@
 :: This file is for Windows only.
 
-mkdir .\bin
-javac .\src\ai\*.java .\src\gui\*.java .\src\client_server\*.java -d .\bin\
+if not exist bin mkdir bin
+javac src\ai\*.java src\gui\*.java src\client_server\*.java -d bin
 :: Then, copy the folder "res\img" to "bin".
-pause;
+pause
