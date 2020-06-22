f7097769c14a40158f4c60d12ea54c4dd3854e71
Christos Kormaris <chriskormaris@gmail.com>
2020-06-22 21:05:47 +0300

    Fix the compile and run ".bat" files.

--- compileAll.bat
+++ compileAll.bat
@@ -2,5 +2,12 @@
 
 if not exist bin mkdir bin
 javac src\ai\*.java src\gui\*.java src\client_server\*.java src\buttons\*.java -d bin
+
 :: Then, copy the folder "res\img" to "bin".
+if not exist bin\img mkdir bin\img
+if not exist bin\img\O mkdir bin\img\O
+if not exist bin\img\X mkdir bin\img\X
+copy res\img\X bin\img\X 
+copy res\img\O bin\img\O
+
 pause
--- runGUI.bat
+++ runGUI.bat
@@ -1,5 +1,2 @@
-:: copy the "img" folder from "res\img" inside the "bin" directory
-if not exist bin/img mkdir bin/img
-cp res/img bin/img
 java -cp bin gui.GUI
 pause
