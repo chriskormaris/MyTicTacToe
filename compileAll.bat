0403e6a25f3d82d01ba41250b26d5ff7545e24cc
Christos Kormaris <chriskormaris@gmail.com>
2020-06-22 23:14:17 +0300

    Compile ".bat" file changes.

--- compileAll.bat
+++ compileAll.bat
@@ -3,7 +3,7 @@
 if not exist bin mkdir bin
 javac src\ai\*.java src\gui\*.java src\client_server\*.java src\buttons\*.java -d bin
 
-:: Then, copy the folder "res\img" to "bin".
+:: Copy the folder "res\img" to "bin".
 if not exist bin\img mkdir bin\img
 if not exist bin\img\O mkdir bin\img\O
 if not exist bin\img\X mkdir bin\img\X
