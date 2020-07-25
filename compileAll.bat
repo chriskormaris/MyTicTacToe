75045b319d3cb7448c33e003ff3c98fcfe33c0a1
Christos Kormaris <chriskormaris@gmail.com>
2020-07-25 15:45:25 +0300

    Update the `.gitignore` file. Restage the file `compileAll.bat`.

--- compileAll.bat
+++ compileAll.bat
@@ -1,13 +1,10 @@
 :: This file is for Windows only.
 
 if not exist bin mkdir bin
-javac src\ai\*.java src\gui\*.java src\client_server\*.java src\buttons\*.java -d bin
+javac src\connect4\*.java src\gui\*.java -d bin
 
-:: Copy the folder "res\img" to "bin".
-if not exist bin\img mkdir bin\img
-if not exist bin\img\O mkdir bin\img\O
-if not exist bin\img\X mkdir bin\img\X
-copy res\img\X bin\img\X 
-copy res\img\O bin\img\O
+:: Copy the folder "res\images" to "bin".
+if not exist bin\images mkdir bin\images
+copy res\images bin\images
 
 pause
