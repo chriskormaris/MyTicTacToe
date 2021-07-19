8e9aff1a5b6e09892cbf333917a879ef72eeecc3
Christos Kormaris <chriskormaris@gmail.com>
2021-07-20 00:08:23 +0300

    Change VERSION from "1.2" to "1.2.0"
    Untrack `MyTicTacToe.jar`

--- src/utility/Constants.java
+++ src/utility/Constants.java
@@ -4,7 +4,7 @@ public class Constants {
 	
 	private Constants() { }  // Prevents instantiation.
 
-	public static final double VERSION = 1.2;
+	public static final String VERSION = "1.2.0";
 
     // Variables for the board values
 	public static final int X = 1;
